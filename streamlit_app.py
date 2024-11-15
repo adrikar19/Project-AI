aa86d603a17f0018afdb62b4094da1233f4fb3da
adrikar19 <adrikar19@gmail.com>
2024-11-15 08:43:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,16 @@
 #git remote add origin https://github.com/adrikar19/Project-AI.git
 #git push -u origin main
 
+
 import streamlit as st
 import time as t
 import pandas as pd
 import numpy as np
+
+st.sidebar.title("LogIn")
+st.sidebar.text_input("UserId")
+st.sidebar.text_input("Password")
+st.sidebar.button("submit")
 
 st.title(":blue[Welcome to AI interview panel]")
 st.header("header")
@@ -23,18 +29,25 @@ st.markdown("proceed")
 st.markdown("### again")
 st.text("text")
 st.caption("caption")
+st.latex(r''' a^2+bx^3''')
 st.checkbox("checkbox")
 st.button("button")
 st.radio("gender",["male","female","other"])
 st.selectbox("select Branch",["CSE","EEE","ECE"])
 st.multiselect("select subject",["C++","Java","HTML"])
+st.select_slider("Grade",["A","B","C"])
+st.slider("mark",0,100)
 st.number_input("Age",0,30)
 st.text_input("Name")
 st.date_input("date of passing")
 st.time_input("time of passing")
 st.text_area("Hobby")
 st.file_uploader("upload CV")
-
+st.color_picker("color")
+st.progress(90)
+with st.spinner("wait"):
+  t.sleep(2)
+st.balloons()
 
 col1, col2, Col3 = st.columns(3)
 
@@ -48,4 +61,8 @@ with Col3:
     st.link_button(":rainbow[Css]", "https://streamlit.io/gallery")
     st.link_button(":rainbow[HTML]", "https://streamlit.io/gallery")
 
+
+
+dataset=pd.dataframe(np.random.randn(50,2),columns=["x","y"])
+st.pie_chart(dataset)
 
